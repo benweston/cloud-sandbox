f4694477529e6cf2866ae96f7b6554f6a93864d2
Ben Weston <bweston26918@gmail.com>
2024-12-05 08:19:47 +0000

    Add versions.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,3 @@
-terraform {
-  required_version = ">= 1.10.0"
-
-  backend "local" {
-    path = "./terraform.tfstate"
-  }
-
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "6.12.0"
-    }
-
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = "6.12.0"
-    }
-  }
-}
-
 provider "google" {
   project = var.project_id
   region  = var.region
---
+++ versions.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.10.0"
+
+  backend "local" {
+    path = "./terraform.tfstate"
+  }
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.12.0"
+    }
+
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "6.12.0"
+    }
+  }
+}
