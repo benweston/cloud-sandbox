03bdd49f80826b29b1dbf52e703fdf507d768243
Ben Weston <bweston26918@gmail.com>
2025-02-18 10:47:43 +0000

    Add initial variables

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "billing_account" {
+  type = string
+  description = "Billing account to use."
+  sensitive = true
+}
+
+variable "region" {
+  type = string
+  description = "Default Google Cloud Region."
+  sensitive = true
+}
+
+variable "project_name" {
+  type = string
+  description = "Project name."
+  sensitive = true
+}
+
+variable "project_id_prefix" {
+  type = string
+  description = "Project ID prefix"
+  sensitive = true
+}
