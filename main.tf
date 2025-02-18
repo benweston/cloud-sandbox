8607ce1a7f9cc7bcf6c4ca7ba2dd76d9289a9e90
Ben Weston <bweston26918@gmail.com>
2025-02-18 11:09:28 +0000

    Add Project Factory

--- main.tf
+++ main.tf
@@ -8,5 +8,29 @@ terraform {
 }
 
 provider "google" {
-  # Configuration options
+  project = 
+  region = var.region
+  zone = var.zone
+}
+
+resource "random_integer" "project_suffix" {
+  min = 000000
+  max = 999999
+}
+
+module "project-factory" {
+  source          = "terraform-google-modules/project-factory/google"
+  version         = "18.0.0"
+  billing_account = var.billing_account
+  name            = var.project_name
+  project_id      = "${var.project_id_prefix}-${random_integer.project_suffix.result}"
+  deletion_policy = "DELETE"
+  activate_apis = [
+    "compute.googleapis.com",
+    "storage.googleapis.com",
+    "iam.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+    "logging.googleapis.com",
+    "monitoring.googleapis.com"
+  ]
 }
